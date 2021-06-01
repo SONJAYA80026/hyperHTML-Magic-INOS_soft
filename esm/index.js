92eff782a81de19d6bbecc6825d981c22873d51a
FFERDIAN TRIAS SONJAYA <ferdiantriassonjayas@outlook.com>
2021-06-02 03:46:33 +0700

    Update index.js

--- esm/index.js
+++ esm/index.js
@@ -13,11 +13,19 @@ import render from './hyper/render.js';
 // you can do the following
 // const {bind, wire} = hyperHTML;
 // and use them right away: bind(node)`hello!`;
-const bind = context => render.bind(context);
+const adopt = context => function () {
+  render.adopt = true;
+  return render.apply(context, arguments);
+};
+const bind = context => function () {
+  render.adopt = false;
+  return render.apply(context, arguments);
+};
 const define = Intent.define;
 const tagger = Tagger.prototype;
 
 hyper.Component = Component;
+hyper.adopt = adopt;
 hyper.bind = bind;
 hyper.define = define;
 hyper.diff = diff;
@@ -41,7 +49,11 @@ setup(content);
 
 // everything is exported directly or through the
 // hyperHTML callback, when used as top level script
+<<<<<<< HEAD
 export {Component, bind, define, diff, hyper, observe, tagger, wire};
+=======
+export {Component, adopt, bind, define, diff, hyper, wire};
+>>>>>>> origin/adopt
 
 // by default, hyperHTML is a smart function
 // that "magically" understands what's the best
@@ -55,7 +67,11 @@ export default function hyper(HTML) {
         ('raw' in HTML ?
           content('html')(HTML) :
           ('nodeType' in HTML ?
+<<<<<<< HEAD
             hyper.bind(HTML) :
+=======
+            bind(HTML) :
+>>>>>>> origin/adopt
             weakly(HTML, 'html')
           )
         )
